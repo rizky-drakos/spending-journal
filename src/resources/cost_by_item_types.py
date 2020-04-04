579742eb30f9d71fdc2e9e2b1de47729615fc756
theminer <nhphat1997@gmail.com>
2020-04-04 15:39:30 +0700

    Modified CostByItemTypes resource
    -CostByItemTypes now supports adding up the cost grouped by item
    types for each month.

--- src/resources/cost_by_item_types.py
+++ src/resources/cost_by_item_types.py
@@ -1,5 +1,6 @@
+from flask import request
 from flask_restful import Resource
-from sqlalchemy import func, cast
+from sqlalchemy import func, cast, extract
 
 from models import ItemModel, ItemTypeModel, CostByItemTypesSchema
 from extentions import db
@@ -10,9 +11,10 @@ cost_by_item_types_schema = CostByItemTypesSchema(many=True)
 
 class CostByItemTypes(Resource):
 
-    def get(self):
+    def post(self):
         cost_by_item_types = \
-        db.session \
-        .query(ItemTypeModel.name, cast(func.sum(ItemModel.amount), db.Integer).label("cost")) \
-        .join(ItemTypeModel.items).group_by(ItemTypeModel.name).all()
+        db.session.query(ItemTypeModel.name, cast(func.sum(ItemModel.amount), db.Integer).label("cost")) \
+        .join(ItemTypeModel.items) \
+        .filter(extract('year', ItemModel.record_date)==request.json["year"], extract('month', ItemModel.record_date)==request.json["month"]) \
+        .group_by(ItemTypeModel.name).all()
         return cost_by_item_types_schema.dump(cost_by_item_types)
