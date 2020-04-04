c1251cbc0f0353e75a149e2c35ea21eb8477ac18
theminer <nhphat1997@gmail.com>
2020-04-04 22:52:47 +0700

    Fixed post method in CostByItemTypes
    -post method of CostByItemTypes did not query the cost data with
    the logged in user's id.

--- src/resources/cost_by_item_types.py
+++ src/resources/cost_by_item_types.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restful import Resource
 from sqlalchemy import func, cast, extract
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from models import ItemModel, ItemTypeModel, CostByItemTypesSchema
 from extentions import db
@@ -11,10 +12,16 @@ cost_by_item_types_schema = CostByItemTypesSchema(many=True)
 
 class CostByItemTypes(Resource):
 
+    @jwt_required
     def post(self):
+        user_id = get_jwt_identity()
         cost_by_item_types = \
         db.session.query(ItemTypeModel.name, cast(func.sum(ItemModel.amount), db.Integer).label("cost")) \
         .join(ItemTypeModel.items) \
-        .filter(extract('year', ItemModel.record_date)==request.json["year"], extract('month', ItemModel.record_date)==request.json["month"]) \
+        .filter(
+            extract('year', ItemModel.record_date) == request.json["year"], 
+            extract('month', ItemModel.record_date) == request.json["month"],
+            ItemModel.user_id == user_id
+        ) \
         .group_by(ItemTypeModel.name).all()
         return cost_by_item_types_schema.dump(cost_by_item_types)
